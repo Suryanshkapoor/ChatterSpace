7f48a88cdacac207fd9f6205d9ad35e557daa1d3
Suryanshkapoor <suryanshkapoorji@gmail.com>
2024-03-10 05:03:46 +0530

    update

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,67 +1,66 @@
-import {createContext, useContext, useEffect, useState} from 'react';
+import { createContext, useContext, useEffect, useState, useRef } from 'react';
 import { getCurrentUser } from '../appwrite/api';
 import { useNavigate } from 'react-router-dom';
 
 export const INITIAL_USER = {
-  id:'',
-  name:'',
-  email:'',
-  imageUrl:'',
-  bio:''
-}
+  id: '',
+  name: '',
+  email: '',
+  imageUrl: '',
+  bio: ''
+};
 
 export const INITIAL_STATE = {
-  user:INITIAL_USER,
+  user: INITIAL_USER,
   isLoading: false,
-  isAuthenticated:false,
-  setUser:()=>{},
-  setIsAuthenticated:()=>{},
-  checkAuthUser:async ()=>false,
-}
+  isAuthenticated: false,
+  setUser: () => {},
+  setIsAuthenticated: () => {},
+  checkAuthUser: async () => false
+};
 
 const AuthContext = createContext(INITIAL_STATE);
 
 const AuthProvider = ({ children }) => {
-
   const [user, setUser] = useState(INITIAL_USER);
   const [isLoading, setIsLoading] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const navigateRef = useRef(navigate); // Create a stable reference to navigate
 
   useEffect(() => {
+    const { current: navigate } = navigateRef; // Destructure the current value from the ref
     const cookieFallback = localStorage.getItem('cookieFallback');
     if (cookieFallback !== '[]' && cookieFallback !== null) {
       navigate('/sign-up');
     } else {
       checkAuthUser();
     }
-  },[]);
-  
-  const checkAuthUser = async() =>{
+  }, [navigateRef]);
+
+  const checkAuthUser = async () => {
     try {
       const currentAccount = await getCurrentUser();
-      if(currentAccount){
+      if (currentAccount) {
         setUser({
-          id:currentAccount.$id,
-          name:currentAccount.name,
-          email:currentAccount.email,
-          imageUrl:currentAccount.imageUrl,
-          bio:currentAccount.bio
-        })
+          id: currentAccount.$id,
+          name: currentAccount.name,
+          email: currentAccount.email,
+          imageUrl: currentAccount.imageUrl,
+          bio: currentAccount.bio
+        });
         setIsAuthenticated(true);
-
         return true;
       }
       return false;
-
     } catch (error) {
       console.log(error);
       return false;
-    }finally{
-      setIsLoading(false)
+    } finally {
+      setIsLoading(false);
     }
-  }
+  };
 
   const value = {
     user,
@@ -70,15 +69,11 @@ const AuthProvider = ({ children }) => {
     isAuthenticated,
     setIsAuthenticated,
     checkAuthUser
-  }
+  };
 
-  return (
-    <AuthContext.Provider value={value}>
-      { children }
-    </AuthContext.Provider>
-  )
-}
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
 
 export default AuthProvider;
 
-export const useUserContext = () => useContext(AuthContext) 
+export const useUserContext = () => useContext(AuthContext);
